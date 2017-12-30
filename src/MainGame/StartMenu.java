f2e0b21e338eb4cc339cd4d2bc7ab9638738219c
Gage <gagefonk@gmail.com>
2017-12-29 17:29:06 -0700

    added a bunch of starting code?

---
+++ src/MainGame/StartMenu.java
@@ -0,0 +1,32 @@
+package MainGame;
+
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class StartMenu {
+
+    //create buttons
+    public Button startGameBtn;
+    public Button exitGameBtn;
+
+    //create obj for MainGame reference
+    MainGame mainGame = new MainGame();
+
+    //start game Button Action
+    public void startGameBtnClicked() throws Exception {
+        mainGame.display();
+        closeStartMenu(startGameBtn);
+    }
+
+
+    //exit game button action
+    public void exitGameBtnClicked(){
+        closeStartMenu(exitGameBtn);
+    }
+
+    //close window for both applications
+    public void closeStartMenu(Button button){
+        Stage stage = (Stage) button.getScene().getWindow();
+        stage.close();
+    }
+}
